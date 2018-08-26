7c66aa9be195c53de2efd2c6e48c0b3380e24a17
Tsubasa Nakayama <app.nakayama@gmail.com>
2018-08-26 19:47:46 +0900

    add sample

--- app/src/main/java/com/github/bassaer/gaid/MainActivity.kt
+++ app/src/main/java/com/github/bassaer/gaid/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.github.bassaer.gaid
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.TextView
+import com.google.android.gms.ads.identifier.AdvertisingIdClient
+import kotlinx.coroutines.experimental.launch
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.main_text)
+        launch {
+            try {
+                val adInfo = AdvertisingIdClient.getAdvertisingIdInfo(applicationContext)
+                textView.text = adInfo.id
+                Log.d(javaClass.name, "GAID -> " + adInfo.id)
+                Log.d(javaClass.name, "TrackingEnabled -> " + adInfo.isLimitAdTrackingEnabled)
+            } catch (e: IOException) {
+                Log.d(javaClass.name, "Error!!!")
+                textView.text = "Error"
+            }
+        }
     }
 }
